cd6a7f3cae5cf1a1cdd746f3d2fbbfbd4a34a13a
Swathi9536 <swathi9536@gmail.com>
2019-04-23 10:12:42 +0530

    first commit

---
+++ src/test/java/gitTest/GITScreenshotTest.java
@@ -0,0 +1,23 @@
+package gitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GITScreenshotTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		WebDriver wd= new FirefoxDriver();
+		wd.manage().window().maximize();		
+		wd.get("https://www.google.co.in/");
+		File src = ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\swathi\\workspace\\GITtest\\Screenshot\\gittest.jpg"));
+	}
+
+}
